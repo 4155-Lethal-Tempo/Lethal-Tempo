bca40178eb50926b773703461c01d02c7e21e050
Cesar Guzman <cesar.castroguz@gmail.com>
2023-10-25 23:30:07 -0400

    Adding ccs and styles to the login page

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
 
+app.use(express.static('public'));
+
 // This page contains a link that sends you to the Spotify login page
 app.get('/', (req, res) => {
     res.render('index.ejs');
